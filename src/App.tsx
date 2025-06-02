887afb0e723338a9648feaeedcf505113a863c97
Atta <uatta6005@gmail.com>
2025-06-02 23:16:41 +0500

    Init commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { Component } from 'react';
+import Calculator from './components/calculator';
+function App(){
+
+  return (
+    <>
+<Calculator/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/calculator.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import { Component } from 'react';
+
+function Calculator(){
+
+    return (
+      <>
+  <div className='w-auto flex justify-center  bg-black text-white text-3xl p-4'>
+    <h1>Caculator</h1>
+  </div>
+  <div>
+
+  </div>
+      </>
+    )
+  }
+  
+  export default Calculator
+  
